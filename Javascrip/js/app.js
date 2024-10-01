ea216a72271eff96e07a56e45f3b534f7d1e1830
AprendizTica <mcvelascou@hotmail.com>
2024-10-01 12:51:29 -0500

    iniciamos Javascript

---
+++ Javascrip/js/app.js
@@ -0,0 +1,76 @@
+console.log ("Hola Mundo lindo dia");
+var nombre = "martha";
+var apellido = "velasco";
+const PI=3.14;
+
+console.log(nombre);
+console.log(apellido);
+console.log(PI);
+
+let persona = {
+nombre:"Ana",
+edad: 30
+
+
+}
+console.log(persona);
+console.log(nombre);
+console.log(persona.logedad)
+
+let personas=["carlos", "pedro", "maria", "juan"]
+
+console.log (personas[3]);
+
+
+function saludar(){   
+    console.log("hola mundo funcion");
+}
+saludar();
+saludar();
+saludar();
+saludar();
+
+let numero1=10;
+let numero2=5;
+let suma = 10 + 5;
+let resta = numero1-numero2;
+multiplicacion = numero1 * numero2;
+let division = numero1/numero2;
+
+console.log("suma", suma);
+console.log(10 + 5 + "suma");
+console.log("suma" + 10 + 5);
+console.log(division);
+
+let x = 10;
+console.log (x);
+x +=5; // => X = X + 5  15
+console.log(x);
+x *=5; // => X = X * 5  75
+console.log(x);
+
+let y = 10;
+console.log (y);
+y++; // => y = y + 1  11
+console.log(y);
+y *=5; // => y = y * 5  55
+console.log(y);
+
+let a = 10;
+let b = 18;
+console.log(a);
+console.log(b);
+
+
+a++; //  => a = a + 5  
+console.log (a != b);
+console.log (a != b);
+console.log (a != b);
+console.log (a != b);
+
+Let m = 10;
+Let n = 18;
+Let o = 10;
+Let p = 12;
+console.log(m ! n y)
+
